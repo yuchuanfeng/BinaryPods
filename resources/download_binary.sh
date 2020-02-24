f1fccc7a6c6a147b346e12af1466e97f9343ec06
yuchuanfeng <yuchuanfeng@baidu.com>
2020-02-24 11:00:36 +0800

    update sh

--- resources/download_binary.sh
+++ resources/download_binary.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
+set -e
+
 PROJECT_NAME=$1
 
-# if [[ -f $PROJECT_NAME/lib/lib$PROJECT_NAME.a ]]; then
-# 	echo "$PROJECT_NAME/lib/lib$PROJECT_NAME.a Exist"
-# 	exit 0
-# fi
+
+
+if [[ -f $PROJECT_NAME/lib/lib$PROJECT_NAME.a ]]; then
+	echo "$PROJECT_NAME/lib/lib$PROJECT_NAME.a Exist"
+	exit 0
+fi
 
 UserName=`git config user.name`
-# https://github.com/yuchuanfeng/BinaryPods.git
-URL="https://github.com/${UserName}/${PROJECT_NAME}.git"
-echo 'PROJECT_NAME' ${PROJECT_NAME}
+
+URL="ssh://${UserName}@icode.baidu.com:8235/baidu/Education-iOS/${PROJECT_NAME}"
+echo 'PROJECT_NAME' $1
 echo 'UserName: ' $UserName
 echo 'URL: '$URL
 
-# cd $PodPath
-# ls -l
-rm -fr lib
+rm -fr $PROJECT_NAME/lib
 mkdir binary_sources
 cd binary_sources
 git clone $URL -b binary --depth=1
 cd ../
-cp -fr binary_sources/$PROJECT_NAME/lib ./
+cp -fr binary_sources/$PROJECT_NAME/lib $PROJECT_NAME/
 rm -fr binary_sources
